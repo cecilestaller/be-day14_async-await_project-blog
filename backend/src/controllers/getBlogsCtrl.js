ed2852987394ca059ac4c3c4994dca4f4208bccf
cecilestaller <c.staller96@gmail.com>
2024-01-24 16:35:23 +0100

    filter für querys in service getAllBlogs.js ausgelagert

--- backend/src/controllers/getBlogsCtrl.js
+++ backend/src/controllers/getBlogsCtrl.js
@@ -8,23 +8,12 @@ async function getBlogsCtrl(req, res) {
         const topic = req.query.topic;
         const titleSearch = req.query.titleSearch;
     
-        const existingBlogs = await BlogService.getAllBlogs();
+        const existingBlogs = await BlogService.getAllBlogs({ topic, titleSearch });
             
-        const blogsResult = existingBlogs
-                    .filter((blog) => {
-                        if(typeof topic === "undefined"){
-                            return true;
-                        }
-                        return blog.topic === topic;
-                    })
-                    .filter((blog) => {
-                        if(typeof titleSearch === "undefined"){
-                            return true;
-                        }
-                        return blog.title.toLowerCase().includes(titleSearch.toLowerCase());
-                });
-        res.status(200).json({ success: true, result: blogsResult })
+        res.status(200).json({ success: true, result: existingBlogs });
+
     } catch(err) {
+        
         console.log(err);
         res.status(500).json({ success: false, error: "failed to load blogs" })
     }
--- backend/src/service/getAllBlogs.js
+++ backend/src/service/getAllBlogs.js
@@ -1,8 +1,21 @@
 const { loadAllBlogs } = require("../data-access")
 
-async function getAllBlogs() {
+async function getAllBlogs(querys) {
     const blogs = await loadAllBlogs();
-    return blogs; 
+    const blogsResult = blogs
+                .filter((blog) => {
+                    if(typeof querys.topic === "undefined"){
+                        return true;
+                    }
+                    return blog.topic === querys.topic;
+                })
+                .filter((blog) => {
+                    if(typeof querys.titleSearch === "undefined"){
+                        return true;
+                    }
+                    return blog.title.toLowerCase().includes(querys.titleSearch.toLowerCase());
+                })
+    return blogsResult; 
 }
 
 // geht hier auch: ohne async-await schreibweise, weil wir mit dem blog-wert nichts machen, sondern nur die promise weitergeben
